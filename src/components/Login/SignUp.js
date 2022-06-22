4f2fa98214b373ea7a5d40eb4022ac6023fc62f0
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-22 23:59:50 +0300

    build signIn screen

--- src/components/Login/SignUp.js
+++ src/components/Login/SignUp.js
@@ -3,7 +3,18 @@ import "../Login/SignUp.css"
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='signupScreen'>
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <button type='submit'>Sign In</button>
+        <h4>
+          <span className='signupScreen__gray'>New to Netflix? </span>
+          <span className='signupScreen__link'>Sign up now.</span>
+        </h4>
+      </form>
+    </div>
   )
 }
 
