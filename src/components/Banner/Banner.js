0ca971da80da6888f44c11d965ac3bb76be7dfb9
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-22 17:04:43 +0300

    Implementing the API

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,26 @@
-import React from 'react';
-import "../Banner/Banner.css"
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import "../Banner/Banner.css";
+import requests from '../../Request';
 
 function Banner() {
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+        Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, [])
+
+  console.log(movie)
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
@@ -11,16 +30,16 @@ function Banner() {
     <header className='banner'
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/2560px-Black_flag.svg.png)`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}>
       <div className='banner__contents'>
-        <h1 className='banner__title'>Movie Name</h1>
+        <h1 className='banner__title'>{movie?.name || movie?.original_name}</h1>
         <div className='banner__buttons'>
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
-        <h1 className='banner__description'>{truncate(`This is a test description.This is a test description.This is a test description.This is a test description.This is a test descriptionThis is a test descriptionThis is a test description.This is a test description`, 150)}</h1>
+        <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
       </div>
       <div className='banner--fadeBottom' />
 
