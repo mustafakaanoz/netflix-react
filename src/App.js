c7da65d96952039d8bfc7b52f5b9d56bb41bf8b1
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-22 23:43:24 +0300

    build login screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen/HomeScreen';
-
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Router, Routes, Link } from "react-router-dom";
+import Login from './components/Login/Login';
 
 function App() {
+
+  const user = null;
+
   return (
     <div className="App">
-      <HomeScreen />
+      <Routes>
+        {!user ? (
+          <Route exact path='/' element={<> <Login /> </>} />
+        ) : (
+          <Route exact path='/' element={<> <HomeScreen /> </>} />
+
+        )}
+
+      </Routes>
     </div>
+
+
   );
 }
 
---
+++ src/components/Login/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../Login/SignUp.css"
+
+function SignUp() {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
