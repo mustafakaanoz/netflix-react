c6ce45cb3222929258fc65ec67fb5e821c353248
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-22 21:43:13 +0300

    firebase deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCVbsXg3gR0acKpVOHXh2nTbKMRT9iGoKM",
   authDomain: "netflix-react-clone-5adad.firebaseapp.com",
@@ -7,3 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "1073731679188",
   appId: "1:1073731679188:web:e60e857704e2dea94f1c41"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
