9acb5a6e007eac252426d04c1727e866208f61a2
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-22 14:47:55 +0300

    build navBar

---
+++ src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../HomeScreen/HomeScreen.css';
+import Nav from '../Nav/Nav';
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      <Nav />
+      {/* Banner */}
+      {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVbsXg3gR0acKpVOHXh2nTbKMRT9iGoKM",
+  authDomain: "netflix-react-clone-5adad.firebaseapp.com",
+  projectId: "netflix-react-clone-5adad",
+  storageBucket: "netflix-react-clone-5adad.appspot.com",
+  messagingSenderId: "1073731679188",
+  appId: "1:1073731679188:web:e60e857704e2dea94f1c41"
+};
